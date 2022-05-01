ce5fc6be31dd3c50048e1115557f6517f6cb1dfd
jairus20 <101726467+jairus20@users.noreply.github.com>
2022-05-01 12:22:06 -0500

    Add files via upload

---
+++ CapaPresentacion/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapaPresentacion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal int saver;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAlumno_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // Llamar a formulario Alumno
+            frmAlumno frm = new frmAlumno();
+
+            //Mostrar el fromulario
+            frm.Show();
+        }
+
+        private void btnDocente_Click(object sender, RoutedEventArgs e)
+        {
+            // Llamar a formulario Administrativo
+            frmDocente frm = new frmDocente();
+
+            //Mostrar el fromulario
+            frm.Show();
+        }
+
+        private void btnAdministrativo_Click(object sender, RoutedEventArgs e)
+        {
+            // Llamar a formulario Administrativo
+            frmAdministrativo frm = new frmAdministrativo();
+
+            //Mostrar el fromulario
+            frm.Show();
+        }
+    }
+}
